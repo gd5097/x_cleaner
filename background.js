684ee83d1afaf7115408eeb4e5d3a44d19b5fb45
jab_cho <gd5097@hanmail.net>
2025-01-30 16:48:45 +0900

    스크립트생성기 escape

--- background.js
+++ background.js
@@ -1,3 +1,21 @@
-chrome.runtime.onInstalled.addListener(() => {
-    console.log("X Cleaner 확장이 설치되었습니다!");
-});
+chrome.webRequest.onBeforeSendHeaders.addListener(
+    (details) => {
+      let headers = {};
+      for (let header of details.requestHeaders) {
+        if (header.name.toLowerCase() === "authorization") {
+          headers["Authorization"] = header.value;
+        } else if (header.name.toLowerCase() === "x-client-transaction-id") {
+          headers["X-Client-Transaction-Id"] = header.value;
+        } else if (header.name.toLowerCase() === "x-client-uuid") {
+          headers["X-Client-Uuid"] = header.value;
+        }
+      }
+  
+      chrome.storage.local.set({ twitterHeaders: headers }, () => {
+        console.log("트위터 헤더 저장됨:", headers);
+      });
+    },
+    { urls: ["https://api.twitter.com/*", "https://api.x.com/*"] },
+    ["requestHeaders"]
+  );
+  
